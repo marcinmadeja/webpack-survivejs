02da79704ef48a8eedc66a7970afa20593168479
jonelet <jonelet@gmail.com>
2017-08-06 20:55:53 +0200

    remove unnecessary code

--- app/component.js
+++ app/component.js
@@ -1,8 +1,5 @@
-import styles from './main.css';
-
 export default (text = 'Hello world') => {
   const element = document.createElement('div');
   element.innerHTML = text;
-  element.className = styles.redButton;
   return element;
 };

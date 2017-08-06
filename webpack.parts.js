86276ea784e8736d6953ac93efc6fed2686a6011
jonelet <jonelet@gmail.com>
2017-08-06 22:46:09 +0200

    Setting Up Autoprefixing

--- webpack.parts.js
+++ webpack.parts.js
@@ -74,3 +74,12 @@ exports.extractCSS = ({ include, exclude, use }) => {
     plugins: [ plugin ],
   };
 };
+
+exports.autoprefix = () => ({
+  loader: 'postcss-loader',
+  options: {
+    plugins: () => ([
+      require('autoprefixer')(),
+    ]),
+  },
+});

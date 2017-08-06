3dc0528181bfb32fbf1ac0d35056c530d1f98090
jonelet <jonelet@gmail.com>
2017-08-06 18:53:51 +0200

    Setting Up webpack-merge

--- webpack.config.js
+++ webpack.config.js
@@ -1,69 +1,45 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const merge = require('webpack-merge');
+
+const parts = require('./webpack.parts');
 
 const PATHS = {
   app: path.join(__dirname, 'app'),
   build: path.join(__dirname, 'build'),
 };
 
-const commonConfig = {
-  // Entries have to resolve to files! They rely on Node
-  // convention by default so if a directory contains *index.js*,
-  // it resolves to that.
-  entry: {
-    app: PATHS.app,
-  },
-  output: {
-    path: PATHS.build,
-    filename: '[name].js',
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      title: 'Webpack demo',
-    }),
-  ],
-};
-
-const productionConfig = () => commonConfig;
-
-const developmentConfig = () => {
-  const config = {
-    devServer: {
-      historyApiFallback: true,
-      stats: 'errors-only',
-      host: process.env.HOST, // Defaults to localhost
-      port: process.env.PORT, // Defaults to 8080
-      overlay: {
-        errors: true,
-        warnings: true,
-      },
+const commonConfig = merge([
+  {
+    entry: {
+      app: PATHS.app,
     },
-    module: {
-      rules: [
-        {
-          test: /\.js$/,
-          enforce: 'pre',
-
-          loader: 'eslint-loader',
-          options: {
-            emitWarning: true,
-          },
-        },
-      ],
+    output: {
+      path: PATHS.build,
+      filename: '[name].js',
     },
-  };
+    plugins: [
+      new HtmlWebpackPlugin({
+        title: 'Webpack demo',
+      }),
+    ],
+  },
+  parts.lintJavaScript({ include: PATHS.app }),
+]);
 
-  return Object.assign(
-    {},
-    commonConfig,
-    config
-  );
-};
+const productionConfig = merge([]);
+
+const developmentConfig = merge ([
+  parts.devServer({
+    host: process.env.HOST,
+    port: process.env.PORT,
+  }),
+]);
 
 module.exports = (env) => {
   if (env === 'production') {
-    return productionConfig();
+    return merge(commonConfig, productionConfig);
   }
 
-  return developmentConfig();
+  return merge(commonConfig, developmentConfig);
 };
---
+++ webpack.parts.js
@@ -0,0 +1,28 @@
+exports.devServer = ({ host, port } = {}) => ({
+  devServer: {
+    historyApiFallback: true,
+    stats: 'errors-only',
+    host,
+    port,
+    overlay: {
+      errors: true,
+      warnings: true,
+    },
+  },
+});
+
+exports.lintJavaScript = ({ include, exclude, options }) => ({
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        include,
+        exclude,
+        enforce: 'pre',
+
+        loader: 'eslint-loader',
+        options,
+      },
+    ],
+  },
+});

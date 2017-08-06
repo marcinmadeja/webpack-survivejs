67708945348986ed56658b50ed9f99c4fd284e81
jonelet <jonelet@gmail.com>
2017-08-06 17:47:23 +0200

    add eslint

--- app/component.js
+++ app/component.js
@@ -2,4 +2,4 @@ export default (text = 'Hello world') => {
   const element = document.createElement('div');
   element.innerHTML = text;
   return element;
-}
+};
--- webpack.config.js
+++ webpack.config.js
@@ -49,4 +49,4 @@ module.exports = (env) => {
   }
 
   return developmentConfig();
-}
+};
